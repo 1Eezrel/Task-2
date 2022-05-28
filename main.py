1fb370598ca0c1f114a2f57412a9c4026c58c5db
1Eezrel <105294553+1Eezrel@users.noreply.github.com>
2022-05-29 00:09:09 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    def isAnagram():
+        first_word = input('tea: ')
+        second_word = input('ate: ')
+print (True if ("first_word" = "second_word")
+  
+
+
+
